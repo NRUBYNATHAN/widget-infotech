9d6007d3b8df0f166a804b25b1eeb361f2490ae1
RUBYNATHAN N <rubynathan999@gmail.com>
2023-10-31 10:49:51 +0530

    log

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
-import Login from "./Components/Login";
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
